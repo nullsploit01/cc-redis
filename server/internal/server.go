6b927d43a09e828f988aab174d0a3237b5a7154d
nullsploit01 <harshaldharmik099@gmail.com>
2024-10-31 23:33:03 +0530

    add ability to accept tcp connections

--- server/internal/server.go
+++ server/internal/server.go
@@ -1,6 +1,10 @@
 package internal
 
-import "net"
+import (
+	"fmt"
+	"io"
+	"net"
+)
 
 type Server struct {
 	port string
@@ -17,4 +21,33 @@ func (s *Server) StartServer() {
 		panic(err)
 	}
 	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("Accepted connection from %s\n", conn.RemoteAddr().String())
+
+	packet := make([]byte, 4096)
+	tmp := make([]byte, 4096)
+
+	for {
+		_, err := conn.Read(packet)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading from client:", err)
+				continue
+			}
+			break
+		}
+		packet = append(packet, tmp...)
+	}
 }
