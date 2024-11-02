600432b9e67d07d5027a30c6d2ddfd12d1b96b5c
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-03 02:07:05 +0530

    fix handleConnection function

--- server/internal/server.go
+++ server/internal/server.go
@@ -18,14 +18,15 @@ func (s *Server) StartServer() error {
 	l, err := net.Listen("tcp", ":"+s.port)
 	fmt.Printf("Listening on port %s\n", s.port)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer l.Close()
 
 	for {
 		c, err := l.Accept()
 		if err != nil {
-			return err
+			fmt.Println("Error accepting connection:", err)
+			continue
 		}
 		go handleConnection(c)
 	}
@@ -35,11 +36,9 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	fmt.Printf("Accepted connection from %s\n", conn.RemoteAddr().String())
 
-	packet := make([]byte, 4096)
-	tmp := make([]byte, 4096)
-
 	for {
-		_, err := conn.Read(tmp)
+		packet := make([]byte, 4096)
+		_, err := conn.Read(packet)
 		if err != nil {
 			if err != io.EOF {
 				fmt.Println("Error reading from client:", err)
@@ -47,7 +46,6 @@ func handleConnection(conn net.Conn) {
 			}
 			break
 		}
-		packet = append(packet, tmp...)
 
 		fmt.Printf("Received packet from %s: %s\n", conn.RemoteAddr().String(), packet)
 	}
