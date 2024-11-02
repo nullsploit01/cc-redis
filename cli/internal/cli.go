dc805a2d966f91e3f70e820d0069e2a80bfb38b8
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-03 01:59:57 +0530

    add ability to send prompts to server from cli

--- cli/internal/cli.go
+++ cli/internal/cli.go
@@ -18,9 +18,31 @@ func (c *CLI) ConnectToServer(host, port string) error {
 	addr := net.JoinHostPort(host, port)
 	conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
 	if err != nil {
-		return fmt.Errorf("could not connect to server at %s", addr)
+		return fmt.Errorf("could not connect to ccredis-server at %s", addr)
 	}
 	defer conn.Close()
 	c.conn = conn
+
+	fmt.Printf("Connected to ccredis-server at %s\n", addr)
+	for {
+		c.Prompt()
+		command := ""
+		fmt.Scan(&command)
+		err := c.SendCommand(command)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (c *CLI) Prompt() {
+	fmt.Print("> ")
+}
+
+func (c *CLI) SendCommand(command string) error {
+	_, err := c.conn.Write([]byte(command))
+	if err != nil {
+		return err
+	}
 	return nil
 }
