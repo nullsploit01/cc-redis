0c9ae45371e75e151018be1100136fe673d5c3ef
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-03 02:51:07 +0530

    Add support for ECHO and PING commands with responses in RESP format

--- server/internal/server.go
+++ server/internal/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strings"
 )
 
 type Server struct {
@@ -42,12 +43,51 @@ func handleConnection(conn net.Conn) {
 		if err != nil {
 			if err != io.EOF {
 				fmt.Println("Error reading from client:", err)
-				continue
 			}
 			break
 		}
 
 		fmt.Printf("Received packet from %s: %s\n", conn.RemoteAddr().String(), packet)
+		response := processCommand(strings.TrimSpace(string(packet)))
+		_, err = conn.Write([]byte(response + "\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to client:", err)
+			break
+		}
+	}
+}
+
+func processCommand(command string) string {
+	parts := strings.Fields(command)
+
+	if len(parts) == 0 {
+		return "-ERR no command received"
 	}
 
+	switch parts[0] {
+
+	case "PING":
+		return "+PONG"
+
+	case "ECHO":
+		if len(parts) < 2 {
+			return "-ERR wrong number of arguments for 'ECHO' command"
+		}
+		return "+" + strings.Join(parts[1:], " ")
+
+	case "SET":
+		if len(parts) != 3 {
+			return "-ERR wrong number of arguments for 'SET' command"
+		}
+		return "+OK"
+
+	case "GET":
+		if len(parts) != 2 {
+			return "-ERR wrong number of arguments for 'GET' command"
+		}
+		return "$-1"
+
+	default:
+		return fmt.Sprintf("-ERR unknown command '%s'", parts[0])
+	}
 }
