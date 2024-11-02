9c588816e810f7e5025e6fd227c03a7aaccc909c
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-03 02:42:32 +0530

    fix structure for sending/reading responses from cli

--- cli/internal/cli.go
+++ cli/internal/cli.go
@@ -1,13 +1,17 @@
 package internal
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 	"time"
 )
 
 type CLI struct {
-	conn net.Conn
+	conn   net.Conn
+	reader *bufio.Reader
 }
 
 func NewCLI() *CLI {
@@ -22,27 +26,54 @@ func (c *CLI) ConnectToServer(host, port string) error {
 	}
 	defer conn.Close()
 	c.conn = conn
+	c.reader = bufio.NewReader(conn)
 
 	fmt.Printf("Connected to ccredis-server at %s\n", addr)
-	for {
-		c.Prompt()
-		command := ""
-		fmt.Scan(&command)
-		err := c.SendCommand(command)
-		if err != nil {
-			return err
-		}
-	}
+	c.Start()
+	return nil
 }
 
-func (c *CLI) Prompt() {
-	fmt.Print("> ")
+func (c *CLI) Start() error {
+	defer c.conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				return err
+			}
+			break
+		}
+
+		command := scanner.Text()
+		if strings.ToUpper(command) == "QUIT" {
+			fmt.Println("Bye!")
+			break
+		}
+
+		if err := c.SendCommand(command); err != nil {
+			return fmt.Errorf("could not send command: %s", err)
+		}
+
+		response, err := c.ReadResponse()
+		if err != nil {
+			return fmt.Errorf("could not read response: %s", err)
+		}
+
+		fmt.Println(response)
+	}
+
+	return nil
 }
 
 func (c *CLI) SendCommand(command string) error {
-	_, err := c.conn.Write([]byte(command))
-	if err != nil {
-		return err
-	}
-	return nil
+	_, err := c.conn.Write([]byte(command + "\r\n"))
+	return err
+}
+
+func (c *CLI) ReadResponse() (string, error) {
+	return c.reader.ReadString('\n')
 }
