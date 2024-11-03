a9f07475158660222c0a0d31548bdcf91b999915
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-03 20:15:40 +0530

    add max number of threads allowed for this server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/nullsploit01/cc-redis/server/cmd"
+import (
+	"runtime"
+
+	"github.com/nullsploit01/cc-redis/server/cmd"
+)
 
 func main() {
+	runtime.GOMAXPROCS(5) // allocates 5 threads
 	cmd.Execute()
 }
